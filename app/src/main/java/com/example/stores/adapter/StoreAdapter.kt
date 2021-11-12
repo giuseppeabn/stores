1b5682cc5cf43f5b0ddc1bc66cee223dc4b0f7a2
Varas Estrada Giuseppe <gvarasex@bice.cl>
2021-11-12 00:27:09 -0300

    add storeAdapter

---
+++ app/src/main/java/com/example/stores/adapter/StoreAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.stores.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.stores.R
+import com.example.stores.databinding.ItemStoreBinding
+import com.example.stores.interfaces.OnClickListener
+import com.example.stores.model.Store
+
+class StoreAdapter(private var stores: MutableList<Store>, private var listener: OnClickListener) :
+    RecyclerView.Adapter<StoreAdapter.ViewHolder>() {
+
+    private lateinit var mContext: Context
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val binding = ItemStoreBinding.bind(view)
+
+        fun setListener(store: Store){
+            binding.root.setOnClickListener { listener.onClick(store) }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        mContext = parent.context
+        val view = LayoutInflater.from(mContext).inflate(R.layout.item_store, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val store = stores[position]
+        with(holder){
+            setListener(store)
+            binding.tvName.text = store.name
+        }
+    }
+
+    override fun getItemCount(): Int = stores.size
+}
