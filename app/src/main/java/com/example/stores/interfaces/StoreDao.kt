d93a0eb2200eb7b82f369d848b6f5553609a9f43
Varas Estrada Giuseppe <gvarasex@bice.cl>
2021-11-12 01:07:47 -0300

    add StoreDao

---
+++ app/src/main/java/com/example/stores/interfaces/StoreDao.kt
@@ -0,0 +1,20 @@
+package com.example.stores.interfaces
+
+import androidx.room.*
+import com.example.stores.model.StoreEntity
+
+@Dao
+interface StoreDao {
+
+    @Query("SELECT * FROM StoreEntity")
+    fun getAllStores() : MutableList<StoreEntity>
+
+    @Insert
+    fun addStore(storeEntity: StoreEntity)
+
+    @Update
+    fun updateStore(storeEntity: StoreEntity)
+
+    @Delete
+    fun deleteStore(storeEntity: StoreEntity)
+}
